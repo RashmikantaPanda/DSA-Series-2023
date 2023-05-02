2173b032e9b05789a98bf088bb9dde2e0c31f9e5
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-05-02 17:50:39 +0530

    SortingArray

--- src/main/java/arrays/SortingArray.java
+++ src/main/java/arrays/SortingArray.java
@@ -1,4 +1,16 @@
 package arrays;
 
 public class SortingArray {
+    public int[] sortArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for (int j=i+1; j<arr.length; j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
--- src/test/java/arrays/SortingArrayTest.java
+++ src/test/java/arrays/SortingArrayTest.java
@@ -1,4 +1,25 @@
-import junit.framework.TestCase;
-public class SortingArrayTest extends TestCase {
-  
+package arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SortingArrayTest {
+
+    @Test
+    public void testSortArray() {
+        int[] arr = {1, 5, 3, 8, 4, 6, 5, 2, 7};
+        SortingArray sortingArray = new SortingArray();
+        int[] actualArray = sortingArray.sortArray(arr);
+        int[] expectedArray = {1, 2, 3, 4, 5, 5, 6, 7, 8};
+        Assert.assertArrayEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    public void testSortArray1(){
+        int[] arr={-8,7,9,6,4,5,2,0,3,-4,0};
+        SortingArray sortingArray=new SortingArray();
+        int[] acturalArray=sortingArray.sortArray(arr);
+        int[] expectedArray={-8,-4,0,0,2,3,4,5,6,7,9};
+        Assert.assertArrayEquals(expectedArray,acturalArray);
+    }
 }
